e30957213c361d7e5ab45b8f0f3e355c9e4e6db4
Matteu <matteugt@gmail.com>
2021-07-29 13:32:27 +0800

    Create landing page
    - SSR not yet needed
    - We can do SEO improvements later down the line :>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './sass/main.css';
+import './sass/modules/_hero.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <main>
+              <div className="hero">
+                  <div className="hero__wrap">
+                      <div>
+                          <h1 className="hero__title"><span className="gtp-pink">Grow</span>Topics</h1>
+                          <h4 className="hero__subtitle">Coming Soon!</h4>
+                      </div>
+                      <div className="hero__actions">
+                          <a href="https://blog.growtopics.xyz" target="_blank" nofollow>
+                              <button className="hero__button sc-button sc-button--filled">
+                                  <span className="sc-button__label">Visit the blog</span>
+                              </button>
+                          </a>
+                          <a href="http://growtopics.xyz/discord" target="_blank" nofollow>
+                              <button className="hero__button sc-button sc-button--outlined">
+                                  <span className="sc-button__label">Discord Server</span>
+                              </button>
+                          </a>
+                      </div>
+                  </div>
+              </div>
+          </main>
+      </div>
   );
 }
 
